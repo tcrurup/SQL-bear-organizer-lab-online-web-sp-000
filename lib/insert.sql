21f3af95781e435e353bdaa195413e5b0159f928
Anthony Rurup <tcrurup@gmail.com>
2019-05-10 20:27:39 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "Male", "Brown", "Nice", 1)
