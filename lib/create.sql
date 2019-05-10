e7928eb4be850378bf7c56ac73e5432121af772b
Anthony Rurup <tcrurup@gmail.com>
2019-05-10 20:24:39 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,6 +1,12 @@
 CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
-    
+    id INTEGER,
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    colot TEXT,
+    temperament TEXT,
+    alive BOOL
 
 
 );
